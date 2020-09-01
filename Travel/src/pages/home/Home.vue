956c8a1e43d2c391f931097ad28609a521e14f5b
xiaxichenqS <xiaxichen@dip-ai.com>
2020-09-02 00:49:16 +0800

    edit Home.vue import header

--- Travel/src/pages/home/Home.vue
+++ Travel/src/pages/home/Home.vue
@@ -1,13 +1,21 @@
 <template>
     <div>
+        <div>
+            <home-header></home-header>
+        </div>
         <div class="home">home</div>
         <router-link to="/list">列表页</router-link>
     </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
+
 export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+        HomeHeader
+    }
 }
 </script>
 
