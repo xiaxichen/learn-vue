2269a87403bd033769a0fdaf248d5cee792c6b95
xiaxichen <xiaxichen1@163.com>
2020-09-02 22:40:14 +0800

    add vue-awesome-swiper@2.6.7 & import swiper.css

--- Travel/src/main.js
+++ Travel/src/main.js
@@ -4,10 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
-import './assets/style/reset.css'
-import './assets/style/border.css'
-import './assets/style/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'style/reset.css'
+import 'style/border.css'
+import 'style/iconfont.css'
+// import style
+import 'swiper/dist/css/swiper.css'
 
+Vue.use(VueAwesomeSwiper)
 Vue.config.productionTip = false
 fastClick.attach(document.body)
 /* eslint-disable no-new */
