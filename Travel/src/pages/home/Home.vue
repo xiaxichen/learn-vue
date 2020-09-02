c1aa85b0b80da9eeca9bdee43d75de13ece0dd8d
xiaxichen <xiaxichen1@163.com>
2020-09-03 00:51:26 +0800

    change layout add HomeIcons lable & delete css .home

--- Travel/src/pages/home/Home.vue
+++ Travel/src/pages/home/Home.vue
@@ -2,9 +2,8 @@
     <div>
         <div>
             <home-header></home-header>
-        </div>
-        <div>
             <home-swiper></home-swiper>
+            <home-icons></home-icons>
         </div>
     </div>
 </template>
@@ -12,18 +11,18 @@
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
 
 export default {
     name: 'Home',
     components: {
         HomeHeader,
-        HomeSwiper
+        HomeSwiper,
+        HomeIcons
     }
 }
 </script>
 
 <style>
-.home {
-    font-size: 20px;
-}
+
 </style>
